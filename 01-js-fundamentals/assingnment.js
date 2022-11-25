0cdf8d7b032757f6515580b41941670930d5abeb
Пётр Куликов <kpetr03job@mail.ru>
2022-11-25 13:48:01 +0500

    Studied data types

--- 01-js-fundamentals/assingnment.js
+++ 01-js-fundamentals/assingnment.js
@@ -1,3 +1,5 @@
+// ASSINGNMENT 1
+console.log('ASSINGNMENT 1');
 let country = 'Russia';
 let continent = 'Eurasia';
 let population = 117;
@@ -5,3 +7,12 @@ let population = 117;
 console.log(country)
 console.log(continent)
 console.log(population)
+
+// ASSINGNMENT 2
+console.log('\n' + 'ASSINGNMENT 2');
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
--- 01-js-fundamentals/script.js
+++ 01-js-fundamentals/script.js
@@ -1,5 +1,18 @@
+let javscriptIsFun = true;
+console.log (javscriptIsFun);
+console.log(typeof javscriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Petr');
+javscriptIsFun = 'yes!';
+console.log(typeof javscriptIsFun);
+let emptyVariable;
+console.log(emptyVariable); 
+
+
+/*
 let js = "amazing";
 
 let firstName = 'Petr';
 console.log(firstName);
 console.log(firstName);
+*/
