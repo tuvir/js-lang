f3f21a1473f604fb3f3c243f8c2358a45e148cd2
Пётр Куликов <kpetr03job@mail.ru>
2022-11-27 09:37:24 +0500

    Worked with if/else trees, challenge 3 done

--- 01-js-fundamentals/challenges-1.js
+++ 01-js-fundamentals/challenges-1.js
@@ -16,3 +16,19 @@ if (markHigherBMI) {
 } else {
     console.log(`John's BMI (${BMIJohn}) is higher than Mark's! (${BMIMark})`);
 }
+
+// CHALLENGE 3
+console.log(`CHALLENGE 3 \n`);
+const coalasScore = (88 + 91 + 110) / 3;
+const dolphinsScore = (96 + 108 + 89) / 3;
+console.log(dolphinsScore, coalasScore);
+if((coalasScore === dolphinsScore) && (dolphinsScore >= 100) && 
+(coalasScore >= 100)) {
+    console.log(`It's a draw!`);
+} else if((coalasScore > dolphinsScore) && (coalasScore >= 100)) {
+    console.log(`Coalas won!`);
+} else if((dolphinsScore > coalasScore) && (dolphinsScore >= 100)) {
+    console.log(`Dolphins won!`);
+} else {
+    console.log(`No one won!`);
+}
