5c1c6fd452238bdefe4df243be29ae85c7ce2d97
Пётр Куликов <kpetr03job@mail.ru>
2022-11-28 17:19:06 +0500

    Array challenge completed.

--- 01-js-fundamentals/part-2/challenges-2.js
+++ 01-js-fundamentals/part-2/challenges-2.js
@@ -20,3 +20,16 @@ const winner = checkWinner(averageDolphins, averageKoalas);
 console.log(winner);
 console.log(averageDolphins, averageKoalas);
 
+//CHALLENGE 2
+console.log(`CHALLENGE 2`);
+function calcTip(billValue) {
+    if(billValue >= 50 && billValue <= 300) {
+       return billValue * 0.15; 
+    } else {
+        return billValue * 0.2;
+    }
+}
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const total = [(bills[0] + tips[0]), (bills[1] + tips[1]), (bills[2] + tips[2])]
+console.log(tips, total);
