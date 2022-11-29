1a5a049b4c9c70c552578e128e449f1e1f953539
Пётр Куликов <kpetr03job@mail.ru>
2022-11-29 16:18:07 +0500

    Worked woth methods that create properties

--- 01-js-fundamentals/part-2/challenges-2.js
+++ 01-js-fundamentals/part-2/challenges-2.js
@@ -21,7 +21,7 @@ console.log(winner);
 console.log(averageDolphins, averageKoalas);
 
 //CHALLENGE 2
-console.log(`CHALLENGE 2`);
+console.log(`\nCHALLENGE 2`);
 function calcTip(billValue) {
     if(billValue >= 50 && billValue <= 300) {
        return billValue * 0.15; 
@@ -33,3 +33,41 @@ const bills = [125, 555, 44];
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 const total = [(bills[0] + tips[0]), (bills[1] + tips[1]), (bills[2] + tips[2])]
 console.log(tips, total);
+
+//CHALLENGE 3
+console.log(`\nCHALLENGE 3`);
+const bmiMark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function() {
+        this.valueBMI = this.mass / this.height ** 2;
+        return this.valueBMI;
+    }
+};
+const bmiJohn = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.95,
+    calcBMI: function() {
+        this.valueBMI = this.mass / this.height ** 2;
+        return this.valueBMI;
+    }
+};
+
+// Calling the method directly
+console.log(bmiJohn.calcBMI(), bmiMark.calcBMI());
+if(bmiJohn.calcBMI() > bmiMark.calcBMI()) {
+    console.log(`${bmiJohn.fullName} BMI (${bmiJohn.calcBMI()}) is higher than ${bmiMark.fullName} (${bmiMark.calcBMI()})`);
+} else {
+    console.log(`${bmiMark.fullName} BMI (${bmiMark.calcBMI()}) is higher than ${bmiJohn.fullName} (${bmiJohn.calcBMI()})`);
+}
+
+// Calling the method first and then calling the property
+bmiJohn.calcBMI();
+bmiMark.calcBMI();
+if(bmiJohn.valueBMI > bmiMark.valueBMI) {
+    console.log(`${bmiJohn.fullName} BMI (${bmiJohn.valueBMI}) is higher than ${bmiMark.fullName} (${bmiMark.valueBMI})`);
+} else {
+    console.log(`${bmiMark.fullName} BMI (${bmiMark.valueBMI}) is higher than ${bmiJohn.fullName} (${bmiJohn.valueBMI})`);
+}
