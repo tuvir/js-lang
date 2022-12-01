435456d01c557a922af38d2d18944fe5d95e70cb
Пётр Куликов <kpetr03job@mail.ru>
2022-12-01 16:55:22 +0500

    Worked with debugger

--- 02-developer-skills/script.js
+++ 02-developer-skills/script.js
@@ -1,2 +1,33 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+/*
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [6, 7, 8, 9, 10];
+const arr3 = arr1.concat(arr1);
+console.log(arr3);
+*/
+
+function convertKelvin() {
+  const measurement = {
+    type: 'temp',
+    unit: 'celcius',
+    // value: Number(prompt('Degrees celsius:')),
+    value: 10,
+  };
+  const kelvinToCelsius = measurement.value + 273;
+  return kelvinToCelsius;
+}
+// 1) IDENTIFY THE BUG
+console.log(convertKelvin());
+
+function calcAmplitude(arr1, arr2) {
+  const temps = arr1.concat(arr2);
+  let max = 0;
+  let min = 0;
+  for (let i = 0; i < temps.length; i++) {
+    const currentValue = temps[i];
+    if (currentValue > max) max = currentValue;
+    if (currentValue < min) min = currentValue;
+  }
+  return max - min;
+}
