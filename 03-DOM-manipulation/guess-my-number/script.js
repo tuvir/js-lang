85319c52efbfab17dfe1017c6710798437667fce
Пётр Куликов <kpetr03job@mail.ru>
2022-12-02 11:30:51 +0500

    Worked with elements selection.

--- 03-DOM-manipulation/guess-my-number/script.js
+++ 03-DOM-manipulation/guess-my-number/script.js
@@ -1,3 +1,7 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Congratulations!';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 18;
+document.querySelector('.highscore').textContent = 11;
