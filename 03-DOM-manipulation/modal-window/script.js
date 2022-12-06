e35dab74954b2faa899b01ffbedcf32aaae42258
Пётр Куликов <kpetr03job@mail.ru>
2022-12-06 16:53:26 +0500

    feat: open/close window by add/remove class

--- 03-DOM-manipulation/modal-window/script.js
+++ 03-DOM-manipulation/modal-window/script.js
@@ -3,3 +3,19 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', openModal);
+}
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
