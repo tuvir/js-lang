58eccd5ac8743397dbe10c6929eee95eaf7549ca
Пётр Куликов <kpetr03job@mail.ru>
2022-12-06 17:27:18 +0500

    feat: close window by  pressing esc button

--- 03-DOM-manipulation/modal-window/script.js
+++ 03-DOM-manipulation/modal-window/script.js
@@ -9,6 +9,11 @@ for (let i = 0; i < btnsShowModal.length; i++) {
 }
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
 
 function openModal() {
   modal.classList.remove('hidden');
@@ -19,3 +24,7 @@ function closeModal() {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 }
+
+// function closeModalEsc(e) {
+
+// }
