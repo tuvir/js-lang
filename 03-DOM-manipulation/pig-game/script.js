4ceb7b8bda23b0c4120e4530b9e28d9c186ce081
Petr Kulikov <p.kulikov.dev@gmail.com>
2022-12-09 19:06:57 +0500

    feat: add hidden class to the dice img

--- 03-DOM-manipulation/pig-game/script.js
+++ 03-DOM-manipulation/pig-game/script.js
@@ -1,20 +1,25 @@
 'use strict';
+// Global variables of players scores
 let player1TotalScore = 0;
 let player2TotalScore = 0;
 let player1CurrentScore = 0;
 let player2CurrentScore = 0;
 let diceRoll = Math.trunc(Math.random() * 6) + 1;
 
+// Variables of score and dice elements
 const player1TotalScoreEl = document.querySelector('#score--0');
 const player2TotalScoreEl = document.querySelector('#score--1');
-player1TotalScoreEl.textContent = player1TotalScore;
-player2TotalScoreEl.textContent = player2TotalScore;
-
 const player1CurrentScoreEl = document.querySelector('#current--0');
 const player2CurrentScoreEl = document.querySelector('#current--1');
+const diceEl = document.querySelector('.dice');
+
+player1TotalScoreEl.textContent = player1TotalScore;
+player2TotalScoreEl.textContent = player2TotalScore;
 player1CurrentScoreEl.textContent = player1CurrentScore;
 player2CurrentScoreEl.textContent = player2CurrentScore;
+diceEl.classList.add('hidden');
 
+// Event handlers
 document.querySelector('.btn--roll').addEventListener('click', rollDicePlayer1);
 
 function rollDicePlayer1() {
