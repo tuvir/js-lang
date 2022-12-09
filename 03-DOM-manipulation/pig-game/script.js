e4f0d2ae8b3f6c2a8b104ad18bc892aa6ad13ae1
Petr Kulikov <p.kulikov.dev@gmail.com>
2022-12-09 18:36:16 +0500

    feat: add global variables for each player score

--- 03-DOM-manipulation/pig-game/script.js
+++ 03-DOM-manipulation/pig-game/script.js
@@ -1,1 +1,13 @@
 'use strict';
+let player1TotalScore = 0;
+let player2TotalScore = 0;
+let player1CurrentScore = 0;
+let player2CurrentScore = 0;
+let diceRoll = Math.trunc(Math.random() * 6) + 1;
+
+document.querySelector('#score--0').textContent = player1TotalScore;
+document.querySelector('#score--1').textContent = player2TotalScore;
+document.querySelector('#current--0').textContent = player1CurrentScore;
+document.querySelector('#current--1').textContent = player2CurrentScore;
+
+document.querySelector('.btn--roll').addEventListener('click', startGame);
