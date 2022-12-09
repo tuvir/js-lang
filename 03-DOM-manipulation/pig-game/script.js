3a5efe48467d0cd932318e60109889e029dc8fee
Petr Kulikov <p.kulikov.dev@gmail.com>
2022-12-09 19:26:07 +0500

    feat: add display dice img function

--- 03-DOM-manipulation/pig-game/script.js
+++ 03-DOM-manipulation/pig-game/script.js
@@ -4,7 +4,6 @@ let player1TotalScore = 0;
 let player2TotalScore = 0;
 let player1CurrentScore = 0;
 let player2CurrentScore = 0;
-let diceRoll = Math.trunc(Math.random() * 6) + 1;
 
 // Variables of score and dice elements
 const player1TotalScoreEl = document.querySelector('#score--0');
@@ -15,25 +14,22 @@ const diceEl = document.querySelector('.dice');
 
 player1TotalScoreEl.textContent = player1TotalScore;
 player2TotalScoreEl.textContent = player2TotalScore;
-player1CurrentScoreEl.textContent = player1CurrentScore;
 player2CurrentScoreEl.textContent = player2CurrentScore;
-diceEl.classList.add('hidden');
 
 // Event handlers
-document.querySelector('.btn--roll').addEventListener('click', rollDicePlayer1);
+document.querySelector('.btn--roll').addEventListener('click', rollDice);
 
-function rollDicePlayer1() {
+function rollDice() {
+  let diceRoll = Math.trunc(Math.random() * 6) + 1;
   displayDice(diceRoll);
-  while (1) {
-    if (diceRoll != 1) {
-      player1CurrentScore += diceRoll;
-      diceRoll = Math.trunc(Math.random() * 6) + 1;
-      displayDice(diceRoll);
-    } else {
-      player1CurrentScore = 0;
-      break;
-    }
+  if (diceRoll !== 1) {
+    player1CurrentScore += diceRoll;
+    player1CurrentScoreEl.textContent = player1CurrentScore;
+    // CHANGE LATER
   }
 }
 
-function displayDice(diceRoll) {}
+function displayDice(diceRoll) {
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dices-img/dice-${diceRoll}.png`;
+}
