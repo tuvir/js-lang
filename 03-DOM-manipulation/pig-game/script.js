738d6108d57756d9e6d5d7a88faed8575511f53e
Petr Kulikov <p.kulikov.dev@gmail.com>
2022-12-09 19:46:11 +0500

    feat: change the variables in roll dice function

--- 03-DOM-manipulation/pig-game/script.js
+++ 03-DOM-manipulation/pig-game/script.js
@@ -1,20 +1,22 @@
 'use strict';
 // Global variables of players scores
+let player0TotalScore = 0;
 let player1TotalScore = 0;
-let player2TotalScore = 0;
+let player0CurrentScore = 0;
 let player1CurrentScore = 0;
-let player2CurrentScore = 0;
 
+let activePlayer = 0;
+let currentScore = 0;
 // Variables of score and dice elements
-const player1TotalScoreEl = document.querySelector('#score--0');
-const player2TotalScoreEl = document.querySelector('#score--1');
-const player1CurrentScoreEl = document.querySelector('#current--0');
-const player2CurrentScoreEl = document.querySelector('#current--1');
+const player0TotalScoreEl = document.querySelector('#score--0');
+const player1TotalScoreEl = document.querySelector('#score--1');
+const player0CurrentScoreEl = document.querySelector('#current--0');
+const player1CurrentScoreEl = document.querySelector('#current--1');
 const diceEl = document.querySelector('.dice');
 
+player0TotalScoreEl.textContent = player0TotalScore;
 player1TotalScoreEl.textContent = player1TotalScore;
-player2TotalScoreEl.textContent = player2TotalScore;
-player2CurrentScoreEl.textContent = player2CurrentScore;
+player1CurrentScoreEl.textContent = player1CurrentScore;
 
 // Event handlers
 document.querySelector('.btn--roll').addEventListener('click', rollDice);
@@ -23,9 +25,11 @@ function rollDice() {
   let diceRoll = Math.trunc(Math.random() * 6) + 1;
   displayDice(diceRoll);
   if (diceRoll !== 1) {
-    player1CurrentScore += diceRoll;
-    player1CurrentScoreEl.textContent = player1CurrentScore;
+    currentScore += diceRoll;
+    document.querySelector(`#current--${activePlayer}`).textContent =
+      currentScore;
     // CHANGE LATER
+  } else {
   }
 }
 
