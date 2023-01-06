c2a2edcd11c6683dd280e5dc17c0e5c81f91a54b
Petr Kulikov <p.kulikov.dev@gmail.com>
2023-01-06 14:52:47 +0500

    feat: variables with same name different scope

--- 04-js-low-level/script.js
+++ 04-js-low-level/script.js
@@ -2,10 +2,13 @@
 
 function calcAge(birthYear) {
   const age = 2023 - birthYear;
+  //   const firstName = 'Petr2';
   function printAge() {
+    // const firstName = 'Petr3';
     const str = `${firstName} is ${age} years old born in ${birthYear}`;
     console.log(str);
     if (birthYear >= 2000 && birthYear <= 2012) {
+      //   const firstName = 'Petr4';
       const generation = `You are a gen Z kid, ${firstName}`;
       console.log(generation);
       function sumNumbers(num1, num2) {
