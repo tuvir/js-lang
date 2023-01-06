3a078a09aeb754f64bf99ea5d57c006ffad63c45
Petr Kulikov <p.kulikov.dev@gmail.com>
2023-01-06 14:37:06 +0500

    feat: worked with block scoped if statement

--- 04-js-low-level/script.js
+++ 04-js-low-level/script.js
@@ -5,6 +5,10 @@ function calcAge(birthYear) {
   function printAge() {
     const str = `${firstName} is ${age} years old born in ${birthYear}`;
     console.log(str);
+    if (birthYear >= 2000 && birthYear <= 2012) {
+      const generation = `You are a gen Z kid, ${firstName}`;
+      console.log(generation);
+    }
   }
   printAge();
   return age;
