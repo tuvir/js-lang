2b3d2b527d2a9dc178caa640af9022e7923653db
Petr Kulikov <p.kulikov.dev@gmail.com>
2023-01-06 14:46:54 +0500

    feat: turn off strict mode for function scope

--- 04-js-low-level/script.js
+++ 04-js-low-level/script.js
@@ -8,8 +8,14 @@ function calcAge(birthYear) {
     if (birthYear >= 2000 && birthYear <= 2012) {
       const generation = `You are a gen Z kid, ${firstName}`;
       console.log(generation);
+      function sumNumbers(num1, num2) {
+        return num1 + num2;
+      }
     }
+    // console.log(sumNumbers(1, 2)); works without strict mode
   }
+  //   console.log(sumNumbers(1, 2));
+
   printAge();
   return age;
 }
