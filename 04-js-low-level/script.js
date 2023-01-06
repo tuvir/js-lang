4756f55530a5ff5e2af2cc617a473d30763677eb
Petr Kulikov <p.kulikov.dev@gmail.com>
2023-01-06 14:31:00 +0500

    feat: add multiple block scopes

--- 04-js-low-level/script.js
+++ 04-js-low-level/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2023 - birthYear;
+  function printAge() {
+    const str = `${firstName} is ${age} years old born in ${birthYear}`;
+    console.log(str);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Petr';
+calcAge(2003);
