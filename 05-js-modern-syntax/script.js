cfaa0774b4957699c052681dbcf0366ce74ece8d
Petr Kulikov <p.kulikov.dev@gmail.com>
2023-01-07 17:58:33 +0500

    feat: destructuring to get multiple function values

--- 05-js-modern-syntax/script.js
+++ 05-js-modern-syntax/script.js
@@ -11,7 +11,9 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
   openingHours: {
     thu: {
       open: 12,
@@ -39,9 +41,12 @@ const c = arr[2];
 // const [x, y, z] = arr;
 // console.log(x, y, z);
 
-let [main, secondary] = restaurant.categories;
-console.log(main, secondary);
+let [primary, secondary] = restaurant.categories;
+console.log(primary, secondary);
 
 // Switching variables with destructuring
-[main, secondary] = [secondary, main];
-console.log(main, secondary);
+[primary, secondary] = [secondary, primary];
+console.log(primary, secondary);
+
+const [starter, main] = restaurant.order(1, 2);
+console.log(`You've ordered ${starter} followed by ${main}`);
