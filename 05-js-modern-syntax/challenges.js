a4c2ee99d83fb8b11d697bbc8d34bd25b74f1436
Petr Kulikov <p.kulikov.dev@gmail.com>
2023-01-09 14:53:22 +0500

    feat: task 2 - 5 of coding challenge 1

--- 05-js-modern-syntax/challenges.js
+++ 05-js-modern-syntax/challenges.js
@@ -42,7 +42,22 @@ const game = {
 };
 
 // TASK 1
-console.log(`TASK 1\n`);
+console.log(`TASK 1`);
 const [players1, players2] = [...game.players];
-
+console.log(players1, players2);
 // TASK 2
+console.log(`\nTASK 2`);
+const [gk, ...fieldPlayers] = [...players1];
+console.log(gk, fieldPlayers);
+// TASK 3
+console.log(`\nTASK 3`);
+const [...allPlayers] = [...game.players];
+console.log(allPlayers);
+// TASK 4
+console.log(`\nTASK 4`);
+const [...players1Final] = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+// TASK 5
+console.log(`\nTASK 5`);
+const { team1, x: draw, team2 } = { ...game.odds };
+console.log(team1, draw, team2);
