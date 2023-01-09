f9c451cf68d90d5899bba6b9ef77ee7cb81328b1
Petr Kulikov <p.kulikov.dev@gmail.com>
2023-01-09 15:47:02 +0500

    feat: coding challenge completed

--- 05-js-modern-syntax/challenges.js
+++ 05-js-modern-syntax/challenges.js
@@ -43,21 +43,37 @@ const game = {
 
 // TASK 1
 console.log(`TASK 1`);
-const [players1, players2] = [...game.players];
+const [players1, players2] = game.players;
 console.log(players1, players2);
 // TASK 2
 console.log(`\nTASK 2`);
-const [gk, ...fieldPlayers] = [...players1];
+const [gk, ...fieldPlayers] = players1;
 console.log(gk, fieldPlayers);
 // TASK 3
 console.log(`\nTASK 3`);
-const [...allPlayers] = [...game.players];
+const allPlayers = [...players1, ...players2];
 console.log(allPlayers);
 // TASK 4
 console.log(`\nTASK 4`);
-const [...players1Final] = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
 console.log(players1Final);
 // TASK 5
 console.log(`\nTASK 5`);
-const { team1, x: draw, team2 } = { ...game.odds };
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
 console.log(team1, draw, team2);
+// TASK 6
+console.log(`\nTASK 6`);
+function printGoals(...players) {
+  for (let i = 0; i < players.length; i++) {
+    console.log(game.scored[i]);
+  }
+  console.log(`Total goals scored: ${players.length}`);
+}
+
+printGoals(...game.scored);
+// TASK 7
+console.log(`\nTASK 7`);
+team1 < team2 && console.log('Team 1 won!');
+team1 > team2 && console.log('Team 2 won!');
